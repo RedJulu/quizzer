23ab8757fba68c83b99cabb3641c1a01a5dd06a7
RedJulu <120446598+RedJulu@users.noreply.github.com>
2024-09-18 09:42:26 +0000

    Javascript Logic start

---
+++ script.js
@@ -0,0 +1,12 @@
+btn1 = document.getElementById("btn1");
+btn2 = document.getElementById("btn2");
+answerDiv = document.getElementById("");
+
+idArray = {};
+questionsArray = {};
+answerArray = {};
+
+function registerQuestion(id, question, answer) {
+    idArray.push(id);
+
+}
