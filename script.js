13656e414227f5b5a8d7018f6c176810e316186e
RedJulu <120446598+RedJulu@users.noreply.github.com>
2024-09-19 08:29:38 +0000

    More Logic

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-btn1 = document.getElementById("btn1");
-btn2 = document.getElementById("btn2");
-answerDiv = document.getElementById("");
+arrayPos = 0;
+startId = 0;
 
-idArray = {};
-questionsArray = {};
-answerArray = {};
+idArray = [];
+questionArray = [];
+answerArray = [];
+answer1Array = [];
+answer2Array = [];
 
-function registerQuestion(id, question, answer) {
-    idArray.push(id);
+function registerQuestion(question, answer, answer1, answer2){
+        startId = startId + 1;
+        idArray.push(startId);
+        questionArray.push(question);
+        answerArray.push(answer);
+        answer1Array.push(answer1);
+        answer2Array.push(answer2);
+}
 
+function answer1(){
+    document.getElementById("div").innerHTML = idArray + questionArray + answerArray + answer1Array + answer2Array;
+}
+
+function answer2(){
+    registerQuestion("Test-Frage", "A", "A", "B");
 }
