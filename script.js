0169033489cc7072a0c075b19aa062f5ceee531d
RedJulu <120446598+RedJulu@users.noreply.github.com>
2024-09-19 09:14:37 +0000

    New Logic Gemini Helped

--- script.js
+++ script.js
@@ -7,73 +7,88 @@ answerArray = [];
 answer1Array = [];
 answer2Array = [];
 
-function registerQuestion(question, answer, answer1, answer2){
-        startId = startId + 1;
-        idArray.push(startId);
-        questionArray.push(question);
-        answerArray.push(answer);
-        answer1Array.push(answer1);
-        answer2Array.push(answer2);
+function registerQuestion(question, answer, answer1, answer2) {
+    startId = startId + 1;
+    idArray.push(startId);
+    questionArray.push(question);
+    answerArray.push(answer);
+    answer1Array.push(answer1);
+    answer2Array.push(answer2);
 }
 
-function getRightAnswer(id){
-    if(idArray.includes(id)){
-        searchValue = idArray.findIndex(id);
-        rightAnswer = answerArray[searchValue];
+function getRightAnswer(id) {
+    if (idArray.includes(id)) {
+        const searchValue = idArray.findIndex(element => element === id);
+        return answerArray[searchValue];
     }
 }
 
-function getAnswer1(id){
-    if(idArray.includes(id)){
-        searchValue = idArray.findIndex(id);
-        rightAnswer = answerArray[searchValue];
+function getAnswer1(id) {
+    if (idArray.includes(id)) {
+        const searchValue = idArray.findIndex(element => element === id);
+        return answer1Array[searchValue];
     }
 }
 
-function getAnswer2(id){
-    if(idArray.includes(id)){
-        searchValue = idArray.findIndex(id);
-        rightAnswer = answerArray[searchValue];
+function getAnswer2(id) {
+    if (idArray.includes(id)) {
+        const searchValue = idArray.findIndex(element => element === id);
+        return answer2Array[searchValue];
     }
 }
 
-function answer1(){
-    if (arrayPos == -1){
+function answer1() {
+    if (arrayPos == -1) {
         arrayPos = 0;
 
-        //Registrieren
+        // Register questions
         registerQuestion("Test-Frage", "A", "A", "B");
         registerQuestion("Test-Frage2", "A", "A", "B");
 
-        //Show
+        // Show question
         document.getElementById("div").innerHTML = questionArray[arrayPos];
         return false;
-    }else{
-        if (getRightAnswer(arrayPos) == getAnswer1(arrayPos)){
+    } else {
+        if (getRightAnswer(arrayPos) == getAnswer1(arrayPos)) {
             document.getElementById("div").innerHTML = "YUP!";
-        }else{
+        } else {
             document.getElementById("div").innerHTML = "Nah! D;";
         }
-    
-        arrayPos = arrayPos + 1;
-    
-       // if (arrayPos == idArray.lenght()){
-         //   return false;
-        //}
-    
-        document.getElementById("div").innerHTML = questionArray[arrayPos];
-    
-        //document.getElementById("div").innerHTML = idArray + questionArray + answerArray + answer1Array + answer2Array;        
-    }
-    
 
+        arrayPos = arrayPos + 1;
+
+        if (arrayPos == idArray.length) {
+            return false;
+        }
+
+        document.getElementById("div").innerHTML = questionArray[arrayPos];
+    }
 }
 
-function answer2(){
-    if (arrayPos == 0){
-        arrayPos = 1;
+function answer2() {
+    if (arrayPos == -1) {
+        arrayPos = 0;
+
+        // Register questions
         registerQuestion("Test-Frage", "A", "A", "B");
+        registerQuestion("Test-Frage2", "A", "A", "B");
+
+        // Show question
+        document.getElementById("div").innerHTML = questionArray[arrayPos];
         return false;
+    } else {
+        if (getRightAnswer(arrayPos) == getAnswer2(arrayPos)) {
+            document.getElementById("div").innerHTML = "YUP!";
+        } else {
+            document.getElementById("div").innerHTML = "Nah! D;";
+        }
+
+        arrayPos = arrayPos + 1;
+
+        if (arrayPos == idArray.length) {
+            return false;
+        }
+
+        document.getElementById("div").innerHTML = questionArray[arrayPos];
     }
-    
 }
